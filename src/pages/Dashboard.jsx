3cef09f83c8f93db1aa21b565d4063495fe59112
Pooja Indulkar <poojaindulkar200@gmail.com>
2023-12-22 00:51:59 +0530

    Dashboard Complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import { DummyPosts } from '../data/data'
-
+import { Link } from 'react-router-dom';
 const Dashboard = () => {
   const [posts, setPosts] = useState(DummyPosts);
   return (
     <section className="dashboard">
-      <div className="container dashboard-container">
-        {posts.length > 0 ? <div className="container posts-container">
+        {posts.length > 0 ? <div className="container dashboard-container">
           {
-            posts.map(({ id, Image, authorId, category, title, des }, index) =>
-              return 
-            )
+            posts.map(({ id, Image, authorId, category, title, des }, index) => {
+              return <article key={index} className="dashboard-post">
+                <div className="dashboard-post-info">
+                  <div className="dashboard-post-image">
+                    <img src={Image} alt="" />
+                  </div>
+                  <h5>{title}</h5>
+                </div>
+                <div className="dashboard-posts-action">
+                  <Link to={`/posts/${authorId}`} className='btn-V btn btn-sm'>View</Link>
+                  <Link to={`/posts/${authorId}/edit`} className='btn btn-sm btn-primary'>Edit</Link>
+                  <Link to={`/posts/${authorId}/delete`} className='btn btn-sm btn-danger'>Delete</Link>
+                </div>
+              </article>
+            })
           }
         </div> : <h2 className='error-center'>You have not made any posts.</h2>}
-      </div>
     </section>
   )
 }
