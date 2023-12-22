9869f19fda73a7692055678a59553ec60fce6888
Pooja Indulkar <poojaindulkar200@gmail.com>
2023-12-22 21:33:49 +0530

    Small Update

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,13 +2,13 @@
 import { useState,React } from 'react'
 import { DummyPosts } from '../data/data'
 import PostItem from './PostItem'
-import HeadPost from './HeadPost'
+import Headpost from './Headpost'
 const Posts = () => {
     const [posts,setPosts]=useState(DummyPosts)
   return (
     <section className='posts'>
         <div className="head-post-aria">
-            <HeadPost/>
+            <Headpost/>
         </div>
        {posts.length >0 ? <div className="container posts-container">
         {
